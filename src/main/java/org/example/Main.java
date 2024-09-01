7e8d89a3e057b7778f8bd7ce0e8f4b8712eaabee
Stefan Yordanov <stefan_yordanov7@abv.bg>
2024-09-01 20:50:18 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BookServiceImpl bookServiceImpl = new BookServiceImpl();
+//        BookServiceImpl bookServiceImpl = new BookServiceImpl();
 //        bookServiceImpl.addBook();
 //        bookServiceImpl.displayBooks();
 
